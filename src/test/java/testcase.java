1e5dd69bef2b88c0d9bedd39e5c398b489d7c4bf
Vikash Sharma <vikashsharma9967@gmail.com>
2024-11-09 13:54:02 +0530

    First Commit

---
+++ src/test/java/testcase.java
@@ -0,0 +1,14 @@
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.asynchttpclient.Response;
+import org.testng.annotations.Test;
+
+public class testcase {
+
+    @Test
+    public void getRequest(){
+        RestAssured.baseURI = "https://reqres.in/";
+        RequestSpecification req= RestAssured.given();
+        Response res = req.get("users?page=
+    }
+}
